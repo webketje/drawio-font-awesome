67e9349f7fd15c3fd0328e5a83cb20aa6674027c
Sarvas, Richard <richard.sarvas@uconn.edu>
2023-02-08 16:31:56 -0500

    build code fix to add ability to change shape colors directly in the Draw.io interface using SVG style hack

--- build.js
+++ build.js
@@ -11,9 +11,28 @@ var svgIcons = fs.readdirSync(srcPath).map(function (p) {
 });
 
 function mxLibraryEntry(svg, color) {
+  // Create a color lookup table to convert web standard color names to hex values
+  // Color names work, but show up incorrectly in the Draw.io interface
+  colorLookup = {
+    black: '#000000',
+    white: '#ffffff',
+    red: '#FF0000',
+    green: '#008000',
+    blue: '#0000FF',
+    navy: '#000080',
+    teal: '#008080',
+    olive: '#808000',
+    purple: '#800080',
+    orange: '#FFA500',
+    brown: '#A52A2A',
+    gray: '#808080',
+  };
+  
   var xml = svg.contents;
   if (color)
-    xml = svg.contents.replace('<path d', '<path fill="' + color.slice(color.indexOf('=') + 1) + '" d');
+    // Modify the path to insert a CSS definition for the path element 
+    // This will later be picked up in Draw.io with the help of the "editableCssRules=path;" style property hack
+    xml = svg.contents.replace('<path d', '<style type="text/css">path {fill:' + colorLookup[color.slice(color.indexOf('=') + 1)] + ';}</style><path d');
   var b64EncodedSvg = Buffer.from(xml).toString("base64");
 
   // Unlike the btoa method in web browsers, the Buffer.toString method can be fed an int array, and
@@ -37,7 +56,8 @@ function mxLibrary(entries) {
 
 function output(color) {
   fs.writeFileSync(
-    path.join(distPath, 'FontAwesome' + (color ? ' - ' + color : '')),
+    // add the file extention to make it easier to open the file
+    path.join(distPath, 'FontAwesome' + (color ? ' - ' + color : '') + '.xml'),
     mxLibrary(svgIcons.map((file, index, arr) => {
       process.stdout.write(" ");
       process.stdout.clearLine();
@@ -50,8 +70,10 @@ function output(color) {
 }
 
 if (process.argv[2] === 'all')
+  // Now that colors can be assigned directly in Draw.io this part may not be needed
+  // Still, someone may want to generate a library with a default color other than black
   [
-    '',
+    'black',
     'white',
     'red',
     'green',
@@ -67,4 +89,5 @@ if (process.argv[2] === 'all')
     output(color);
   });
 else
-  output(color);
+  // Provide a backstop color value since color values not in the lookup table will cause issues
+  output('black');
